1a786d170b2bd62ad724e3dde607920d4fce7341
Mazharul77 <mazharul77.cse@gmail.com>
2022-03-05 00:45:24 +0600

    swapping variables simply

---
+++ swapping_pair.py
@@ -0,0 +1,7 @@
+text_1 = "Seldom"
+text_2 = "Bites"
+print("Initial String ", "text_1=", text_1, "and text_2=", text_2)
+
+text_1, text_2 = text_2, text_1
+print("\nAfter Swapped String ", "text_1=", text_1, "and text_2=", text_2)
+
